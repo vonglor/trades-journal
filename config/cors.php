406ebdec0fcf4e011a7faba2913eb57a42ec2f7f
vonglor patou <vonglorpatou@vonglors-Laptop.local>
2026-07-11 12:43:36 +0700

    origin

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['*'],
+    'allowed_origins' => ['https://tradesjournal.vonglorpatou.workers.dev'],
 
     'allowed_origins_patterns' => [],
 
